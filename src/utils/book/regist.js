98a4e9dccf42f1a6e4e476ed68eaea77077fa84f
MS Kang <tomskang@naver.com>
2025-03-15 00:25:25 +0900

    update regist.js logic

--- src/utils/book/regist.js
+++ src/utils/book/regist.js
@@ -15,36 +15,36 @@ export async function registerBook(bookData) {
     ]);
     if (!coverFileId || !contentFileId) {
       throw new Error(`File not found in Google Drive: /stage/${bookData.cover} || /stage/${bookData.content}`); }
-
-    // Upload to AWS S3
     const coverExt = bookData.cover.split(".").pop();
     const contentExt = bookData.content.split(".").pop();
-    const [coverS3Url, contentS3Url] = await Promise.all([
-      copyDriveFile(coverFileId, "cover", `${newId}.${coverExt}`),
-      copyDriveFile(contentFileId, "content", `${newId}.${contentExt}`)
-    ]);    
 
-    // Store book data in DB
-    await Promise.all([
-      createBook({
-        id: newId,
-        title: bookData.title,
-        edition: bookData.edition,
-        author: bookData.author,
-        cover: coverS3Url,
-        content: contentS3Url,
-        coverFileName: `${newId}.${coverExt}`,
-        contentFileName: `${newId}.${contentExt}`,
-        tags: bookData.tags,
-        mainTags: bookData.mainTags,
-        createdAt: new Date().toISOString(),
-      }),
-      updateTagDB([...bookData.tags, ...bookData.mainTags], newId),
+    // Define contentUrl promise
+    const contentUrlPromise = contentExt === "pdf"
+      ? moveDriveFile(contentFileId, `/content/${newId}.${contentExt}`).then(() => 
+          `https://drive.google.com/file/d/${contentFileId}/view`)
+      : copyDriveFile(contentFileId, "content", `${newId}.${contentExt}`);
+    const [coverS3Url, contentUrl] = await Promise.all([
+      copyDriveFile(coverFileId, "cover", `${newId}.${coverExt}`),
+      contentUrlPromise,
     ]);
 
-    // Move files in Google Drive (async, no need to wait)
-    moveDriveFile(coverFileId, `/cover/${newId}.${coverExt}`);
-    moveDriveFile(contentFileId, `/content/${newId}.${contentExt}`);
+    // Store book data in DB
+    await createBook({
+      id: newId,
+      title: bookData.title,
+      edition: bookData.edition,
+      author: bookData.author,
+      cover: coverS3Url,
+      content: contentUrl,
+      coverFileName: `${newId}.${coverExt}`,
+      contentFileName: `${newId}.${contentExt}`,
+      tags: bookData.tags,
+      mainTags: bookData.mainTags,
+      createdAt: new Date().toISOString(),
+    });
+
+    // Move cover file in Google Drive
+    await moveDriveFile(coverFileId, `/cover/${newId}.${coverExt}`);
 
     console.log(`return ${newId}`);
     return newId;
