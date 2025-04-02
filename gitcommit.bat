00643036b77938802099c01c469ff25120623188
MS Kang <tomskang@naver.com>
2025-04-02 17:57:45 +0900

    feat: gitcommit.bat이 git push origin master 또한 함께 수행하도록 변경

--- gitcommit.bat
+++ gitcommit.bat
@@ -3,4 +3,4 @@ set /p commit_message="Commit message: "
 git add .
 git commit -m "%commit_message%"
 for /f "tokens=*" %%b in ('git branch --show-current') do set branch=%%b
-@REM git push -u origin %branch%
+git push -u origin %branch%
