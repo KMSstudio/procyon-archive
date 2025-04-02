7d72823ed14fdd0845ad6446a5b1a5c95e72c258
MS Kang <tomskang@naver.com>
2025-04-02 10:23:27 +0900

    chore: gitcommit.bat에서 자동 git push 기능 제거

--- gitcommit.bat
+++ gitcommit.bat
@@ -3,4 +3,4 @@ set /p commit_message="Commit message: "
 git add .
 git commit -m "%commit_message%"
 for /f "tokens=*" %%b in ('git branch --show-current') do set branch=%%b
-git push -u origin %branch%
+@REM git push -u origin %branch%
