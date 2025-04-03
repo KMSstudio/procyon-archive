91bda95ed05228cdb9100735eada97fd3370487e
MS Kang <tomskang@naver.com>
2025-04-03 12:53:07 +0900

    delete: logger.js flush schedule 삭제

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,7 +6,6 @@ class Logger {
   constructor() {
     this.buffer = [];
     this.flushInterval = null;
-    this.#initFlushTimers();
   }
 
   log(msg) { this.write("LOG", msg); }
@@ -14,45 +13,13 @@ class Logger {
   error(msg) { this.write("ERROR", msg); }
 
   write(type, msg) {
+    console.log(`${JSON.stringify(this.buffer)}`)
     const now = new Date().toISOString();
     this.buffer.push(`[${now}] ${type}: ${msg}`);
   }
 
-  #initFlushTimers() {
-    this.#scheduleHourlyFlush();
-    this.#scheduleDailyFlush();
-  }
-
-  #scheduleHourlyFlush() {
-    const intervalHour = parseInt(process.env.LOGGER_HOUR_INTERVAL || "2"); // default to 2
-    const now = new Date();
-    const next = new Date(now);
-  
-    next.setMinutes(0, 0, 0);
-    const nextHour = Math.ceil(now.getHours() / intervalHour) * intervalHour;
-    next.setHours(nextHour);
-  
-    const msUntilNext = next - now;
-    setTimeout(() => {
-      this.flush("hourly");
-      setInterval(() => this.flush("hourly"), 1000 * 60 * 60 * intervalHour);
-    }, msUntilNext);
-  }
-
-  #scheduleDailyFlush() {
-    const now = new Date();
-    const nextDay = new Date(now);
-    nextDay.setHours(0, 0, 0, 0);
-    nextDay.setDate(now.getDate() + 1);
-
-    const msUntilMidnight = nextDay - now;
-    setTimeout(() => {
-      this.flush("daily");
-      setInterval(() => this.flush("daily"), 1000 * 60 * 60 * 24); // every midnight
-    }, msUntilMidnight);
-  }
-
   flush(type = "manual") {
+    console.log(`flush type = ${type}`);
     if (this.buffer.length === 0) return null;
 
     const content = this.buffer.join("\n") + "\n";
