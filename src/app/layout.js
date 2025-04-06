190a089e6ac678ae424cb0dafd454a0d5a5efb60
MS Kang <tomskang@naver.com>
2025-04-06 21:55:04 +0900

    fix: 배너 이미지를 erin.png로 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,14 +11,14 @@ export const metadata = {
   description: "경성제국대학 전자계산기학과의 아카이브",
   openGraph: {
     title: "CSE Archive",
-    description: "경성제국대학 전자계산기학과의 아카이브",
+    description: "경성제대 컴공과 아카이브",
     url: "https://cse-archive.com",
     siteName: "CSE Archive",
     images: [
       {
-        url: "/image/opengraph/banner.png",
-        width: 800,
-        height: 600,
+        url: "/image/opengraph/erin.png",
+        width: 1200,
+        height: 630,
         alt: "CSE Archive 페이지",
       },
     ],
