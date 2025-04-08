6324f72ae369d322f55a11fd4fc87034d9ef378e
MS Kang <tomskang@naver.com>
2025-04-08 12:18:18 +0900

    getUser를 getUserv2로 업데이트 및 updateUserAccess > updateUserAccessDate 로 기능 개선

--- src/app/drive/layout.js
+++ src/app/drive/layout.js
@@ -7,20 +7,20 @@ import NavBar from "@/app/components/NavBar";
 import NoLoginComponent from "@/app/drive/no-login";
 // Constants
 import navData from "@/config/navConstant.json";
-import { getUser } from "@/utils/auth";
-import { updateUserAccess } from "@/utils/database/userDB"
+import { getUserv2 } from "@/utils/auth";
+import { updateUserAccessDate } from "@/utils/database/userDB"
 // Style (CSS)
 import "@/styles/drive.css";
 
 const allowedBots = [ "Googlebot", "Bingbot", "DuckDuckBot", "Yeti", "Daumoa", "Applebot" ];
 
 export default async function DriveLayout({ children }) {
-  const userData = await getUser();
+  const userData = await getUserv2();
 
   const ua = headers().get("user-agent") || "";
   const isAllowedBot = allowedBots.some(bot => ua.includes(bot));
-  const content = (userData.do_user_login || isAllowedBot) ? children : <NoLoginComponent />;
-  // updateUserAccess(userData.user_email, userData.user_info);
+  const content = (userData.login || isAllowedBot) ? children : <NoLoginComponent />;
+  if (!isAllowedBot) { updateUserAccessDate(userData.email); }
 
   return (
     <div className="main-container">
