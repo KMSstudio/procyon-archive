9d8c3216f123e31d8c644a681351ed0090d581fc
MS Kang <tomskang@naver.com>
2025-04-08 12:23:42 +0900

    delete: 크롤러 접근 허용을 다시 막음. 1. Client Component가 주 컨텐트고, 2. 도서 업로드가 검색되봐야 좋은 꼴 못 봄

--- src/app/drive/layout.js
+++ src/app/drive/layout.js
@@ -1,7 +1,5 @@
 /* @/app/drive/layout.js */
 
-// Header infomation for User-agent
-import { headers } from "next/headers";
 // Components
 import NavBar from "@/app/components/NavBar";
 import NoLoginComponent from "@/app/drive/no-login";
@@ -12,15 +10,10 @@ import { updateUserAccessDate } from "@/utils/database/userDB"
 // Style (CSS)
 import "@/styles/drive.css";
 
-const allowedBots = [ "Googlebot", "Bingbot", "DuckDuckBot", "Yeti", "Daumoa", "Applebot" ];
-
 export default async function DriveLayout({ children }) {
   const userData = await getUserv2();
-
-  const ua = headers().get("user-agent") || "";
-  const isAllowedBot = allowedBots.some(bot => ua.includes(bot));
-  const content = (userData.login || isAllowedBot) ? children : <NoLoginComponent />;
-  if (!isAllowedBot) { updateUserAccessDate(userData.email); }
+  const content = userData.login ? children : <NoLoginComponent />;
+  updateUserAccessDate(userData.email);
 
   return (
     <div className="main-container">
