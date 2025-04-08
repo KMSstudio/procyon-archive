eab52ed0f520031fa8e3b927033c32c9ecf21c8a
MS Kang <tomskang@naver.com>
2025-04-08 12:14:16 +0900

    feat: drive/layout.js에서, 브라우저 크롤러의 접속을 허용 - 허용 리스트는 layout.js의 allowedBots 참조

--- src/app/drive/layout.js
+++ src/app/drive/layout.js
@@ -1,5 +1,7 @@
 /* @/app/drive/layout.js */
 
+// Header infomation for User-agent
+import { headers } from "next/headers";
 // Components
 import NavBar from "@/app/components/NavBar";
 import NoLoginComponent from "@/app/drive/no-login";
@@ -10,10 +12,15 @@ import { updateUserAccess } from "@/utils/database/userDB"
 // Style (CSS)
 import "@/styles/drive.css";
 
+const allowedBots = [ "Googlebot", "Bingbot", "DuckDuckBot", "Yeti", "Daumoa", "Applebot" ];
+
 export default async function DriveLayout({ children }) {
   const userData = await getUser();
-  const content = userData.do_user_login ? children : <NoLoginComponent />;
-  updateUserAccess(userData.user_email, userData.user_info);
+
+  const ua = headers().get("user-agent") || "";
+  const isAllowedBot = allowedBots.some(bot => ua.includes(bot));
+  const content = (userData.do_user_login || isAllowedBot) ? children : <NoLoginComponent />;
+  // updateUserAccess(userData.user_email, userData.user_info);
 
   return (
     <div className="main-container">
