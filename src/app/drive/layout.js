661f6b7a23309d480e183c2881f721d1834c550a
MS Kang <tomskang@naver.com>
2025-04-21 01:34:51 +0900

    feat: check is test envinorment and do not check user login when it is on test env

--- src/app/drive/layout.js
+++ src/app/drive/layout.js
@@ -12,7 +12,7 @@ import "@/styles/drive.css";
 
 export default async function DriveLayout({ children }) {
   const userData = await getUserv2();
-  const content = userData.login ? children : <NoLoginComponent />;
+  const content = (userData.login || process.env.NODE_ENV === "test") ? children : <NoLoginComponent />;
   updateUserAccessDate(userData.email);
 
   return (
