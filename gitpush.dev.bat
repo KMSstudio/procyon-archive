facb3f13c5c63ee9d624916a6063c5895df7418c
Kang Myeong Seok <tomskang@naver.com>
2025-06-27 13:54:07 +0900

    fix: gitpush.dev.bat에 커밋 추가 기능 수정

--- gitpush.dev.bat
+++ gitpush.dev.bat
@@ -1,6 +1,3 @@
 @echo off
-set /p commit_message="Commit message: "
-git add .
-git commit -m "%commit_message%"
 for /f "tokens=*" %%b in ('git branch --show-current') do set branch=%%b
 git push origin %branch%
