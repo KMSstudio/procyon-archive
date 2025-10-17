b34f1b572188f91254265bf464dbdc673f86d07a
Kang Myeong Seok <tomskang@naver.com>
2025-10-17 15:00:15 +0900

    feat: update book/layout.jsx logic

--- src/app/drive/book/layout.jsx
+++ src/app/drive/book/layout.jsx
@@ -6,11 +6,12 @@ import NoAdminComponent from "@/app/drive/no-admin";
 import { getUserv2 } from "@/utils/auth";
 
 export default async function BookLayout({ children }) {
-  // const userData = await getUserv2();
-  
-  // if (
-  //   (process.env.BOOKPAGE_PUBLIC !== "TRUE") && 
-  //   (!userData?.admin && !userData?.prestige)
-  // ) { return <NoAdminComponent />; }
-  return <>{children}</>;
+  if (process.env.BOOKPAGE_PUBLIC === "TRUE") {
+    return <>{children}</>;
+  } else {
+    const userData = await getUserv2();
+    return (!userData?.admin && !userData?.prestige)
+      ? <NoAdminComponent />
+      : <>{children}</>;
+  }  
 }
