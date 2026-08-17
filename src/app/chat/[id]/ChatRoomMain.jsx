600e1c798f0a6723e525a94115df7f5beab906b8
Kang MyeongSeok <tomskagn@naver.com>
2026-08-17 16:45:21 +0900

    Componentize chat pages

---
+++ src/app/chat/[id]/ChatRoomMain.jsx
@@ -0,0 +1,33 @@
+/* app/chat/[id]/ChatRoomMain.js */
+
+import ChatRoom from "./ChatRoom";
+import { getUserv2 } from "@/utils/auth";
+import { anonymizeChat, anonymizeSender } from "@/utils/chat/anonymize";
+import { fetchRoom, fetchMessages } from "@/utils/database/chatDB";
+import { redirect } from "next/navigation";
+
+export default async function ChatRoomMain({ roomId }) {
+  const userData = await getUserv2();
+  if (!userData.login) { redirect("/"); }
+
+  const [room, msg] = await Promise.all([
+    fetchRoom(roomId),
+    fetchMessages(roomId),
+  ]);
+  const annMsg = msg.map(anonymizeChat);
+  const annUser = anonymizeSender({ email: userData.email, name: userData.name, major: userData.major });
+
+  const cursor = {
+    oldest: msg.length ? msg[0].createdAt : null,
+    latest: msg.length ? msg[msg.length - 1].createdAt : null,
+  };
+
+  return (
+    <ChatRoom
+      annUser={annUser}
+      room={room}
+      annMsg={annMsg}
+      cursor={cursor}
+    />
+  );
+}
